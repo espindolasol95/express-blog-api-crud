eda47d5a25d2cb047d2c72a9228e9ebec303eb7d
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-04 21:52:48 +0200

    feat(aggiunta di  ID agli oggetti del array per avere un identificatore di ogni oggetto)

--- data/postsData.js
+++ data/postsData.js
@@ -1,30 +1,30 @@
 const posts =[
     
-    {
+    {   id: 1,
         titolo:'Mandalorian',
         contenuto:'lorem ipsum dolor',
         immagine:'img/mando.webp',
         tags:['forza']
     },
-    {
+    {   id: 2,
         titolo:'Mando e grogu',
         contenuto:'lorem ipsum dolor',
         immagine:'img/a.jpg',
         tags:['forza','tenerezza']
     },
-    {
+    {   id: 3,
         titolo:'Boba fet',
         contenuto:'lorem ipsum dolor',
         immagine:'img/s-l1200.jpg',
         tags:['cacciatore, abilità']
     },
-    {
+    {   id: 4,
         titolo:'Elmo',
         contenuto:'lorem ipsum dolor',
         immagine:'img/elmo-png.png',
         tags:['simpatico']
     },
-    {
+    {   id: 5,
         titolo:'Messi',
         contenuto:'lorem ipsum dolor',
         immagine:'img/Leo-messi-PNG.png',
